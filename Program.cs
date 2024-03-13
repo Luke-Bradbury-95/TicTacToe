5fc0cab2e8040790f1582791115f1740eadfe4bb
luke-bradbury-95 <luke-bradbury-95@hotmail.com>
2024-03-13 20:12:54 +0000

    Added the template

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
 ﻿
+SetField();
+
+
+
+
+static void SetField()
+{
+    Console.WriteLine("     |     |     ");
+    Console.WriteLine("  {0}  |  {1}  |   {2} ", 1, 2, 3);
+    Console.WriteLine("_____|_____|____");
+    Console.WriteLine("     |     |     ");
+    Console.WriteLine("  {0}  |  {1}  |   {2} ", 1, 2, 3);
+    Console.WriteLine("_____|_____|____");
+    Console.WriteLine("     |     |     ");
+    Console.WriteLine("  {0}  |  {1}  |   {2} ", 1, 2, 3);
+    Console.WriteLine("_____|_____|____");
+    Console.WriteLine("     |     |     ");
+}
+Console.ReadKey();
